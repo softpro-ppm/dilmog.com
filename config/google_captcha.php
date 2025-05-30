3d98af50f30db54c603e97393ff2b23ea1a0ff53
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-05-30 16:41:54 +0530

    Update google_captcha.php

--- config/google_captcha.php
+++ config/google_captcha.php
@@ -3,8 +3,8 @@
 ///  zidrop.com
 
 return [
-    'site_key' => env('GCAPTCHA_SITE_KEY', '6LcrDKYjAAAAAAiEjKLLW9KxcR4lNwb8d2Wpd4Hl'),
-    'secret_key' => env('GCAPTCHA_SECRET_KEY', '6LcrDKYjAAAAAP4LPKigylYnKV9k-CitOj54kK6r')
+    'site_key' => env('GCAPTCHA_SITE_KEY', '6LewNFArAAAAAB46wPwkSqRuLiHpBnQ0hU63P5qR'),
+    'secret_key' => env('GCAPTCHA_SECRET_KEY', '6LewNFArAAAAAPPXgJE_nIp4KKNvx0Wpj80m8Cx_')
 ];
 
 
